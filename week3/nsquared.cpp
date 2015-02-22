3346cfe66fb0636ef95fc5749df1e3ff70c86609
NerdiusMaximus <mtl6@njit.edu>
2015-02-22 16:53:41 -0500

    Added nsquared.cpp and the binary compiled file.
    this program computes a table of N and N squared values and displays it using a for loop construct.

---
+++ week3/nsquared.cpp
@@ -0,0 +1,54 @@
+/*
+Intro to C/C++ Programming
+Spring 2015
+INFO1-9008-01
+
+Week One
+Programming Homework
+
+Author: Michael T. Lowry
+Date: 02/22/2015
+
+These are the program examples for Looping and decision Tree chapters
+
+Chapter 5 Excercises 2,4,6,11
+Chapter 6 Excercises 2, 3, 6
+*/
+
+//Chapter 5 Excercise 2
+
+/*
+Write a program to generate and display a table of N and N^2 
+for integer values of n ranging from 1 to 10, using 
+appropriate column headers and output formats
+
+output: 
+
+Table of squares
+Value of N | Value of N^2
+-------------------------
+N = 1	|	N^2 = 1
+N = 2	|	N^2 = 4
+...
+N = 10	|	N^2 = 100
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	//Initialize counter N
+	int n = 1, n2;
+	
+	//Print Table Header
+	printf("Table of Squares\nValue of N\t|\tValue of N^2\n----------------------\n");
+	
+	//Execute a loop to calculate the value of n ^2 and display in a table
+	for(n=1;n <=10; n++)
+	{
+		n2 = n*n;
+		printf("N = %i\t|\tN^2= %i\n",n,n2);		
+	}//end for
+
+return 0;
+}//end main

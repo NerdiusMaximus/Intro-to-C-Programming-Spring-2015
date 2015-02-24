08ae2a955d19758984814fec931a275e90b55e5e
NerdiusMaximus <mtl6@njit.edu>
2015-02-23 22:03:40 -0500

    Changed folder structure

---
+++ week2/hellopi.c
@@ -0,0 +1,24 @@
+/*  
+Intro to C/C++ Programming
+Spring 2015
+INFO1-CE9008-01
+
+Week one
+Program Example: Hellow World
+
+Author: Michael T Lowry
+Date: 02/08/2015
+
+This program tests the compilation and running of a program on a raspberry pi to print Hello World
+*/
+#include <stdio.h>
+
+int main(void)
+{
+	printf("\nHello World!\n\nProgramming in C is fun. \nProgramming C on a Raspberry Pi is even more fun \n\n");
+
+	printf("This code will be saved in a github repository.\n\n");
+
+	return 0;
+}
+
---
+++ week2/promo.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	float x;
+	x = 7.0;
+	int y = (int)x;
+	int z =  (int)x;
+	int q = (float)x;
+	printf("x = %i\n",x);
+ 
+	return 0;
+}

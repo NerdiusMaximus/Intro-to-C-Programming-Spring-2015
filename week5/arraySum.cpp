e06cb0558d005046b7ff13a1efe8444f1ef6c710
Michael Lowry <mtl6@njit.edu>
2015-03-11 00:43:52 -0400

    Added solution to Exercise 11
    Solved the problem for a function that sums the elements of an array.
    
    Used a simple array 0 - 9 to demonstrate

--- week5/arraySum.cpp
+++ week5/arraySum.cpp
@@ -17,5 +17,51 @@ Chapter 13 Excercises 3, 4, 5, 9
 //Chapter 8 Excercise 11
 
 /*
-Write a function that 
+Write a function called arraySum that takes two arguments: 
+an integer array and thnumber of elements in the array. 
+Have the function return as a result the sum of the elements in the array.
+*/
+
+#include <stdio.h>
+
+long int arraySum(int array[], int n)
+{
+	long int sum = 0;
+	for (int i=0;i<n;++i)
+	{
+		sum = sum + array[i];
+	}
+	
+	return sum;
+}
+
+int main(void)
+{
+	int n = 10,sum = 0;
+	int array[10] = {0,1,2,3,4,5,6,7,8,9};
+	
+	sum = arraySum(array,n);
+	
+	printf("The array is:\n");
+	
+	for(int i=0;i<n;++i)
+	{
+		printf("%i\t",array[i]);
+	}
+	printf("\n\n");
+	
+	printf("The sum of the array is %i",sum);
+	
+	return 0;
+}
+
+/* Output
+The array is:
+0       1       2       3       4       5       6       7       8       9
+
+
+The sum of the array is 45
+--------------------------------
+Process exited after 0.06893 seconds with return value 0
+Press any key to continue . . .
 */

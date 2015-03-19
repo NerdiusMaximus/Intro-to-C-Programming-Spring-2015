9725f045a2107d7431bbf04d29e3e176d233cc39
Michael Lowry <mtl6@njit.edu>
2015-03-18 22:08:53 -0400

    Added solution for part 1 of quiz 2
    Length of a string using pointers was implemented with pointers.

---
+++ quiz2.cpp
@@ -0,0 +1,88 @@
+/*
+Intro to C/C++ Programming
+Spring 2015
+INFO1-9008-01
+
+Week Six
+
+Programming Quiz 2
+
+Author: Michael T. Lowry
+Date: 03/18/2015
+
+Quiz 2 prompt:
+
+You are given a character string  containing 
+the following prose: 
+"To be or not to be - that is the question: 
+Whether 'tis nobler in the mind to suffer 
+the slings and arrows of outrageous fortune, 
+or to take arms against a sea of troubles and,
+ by opposing, end them. To die, to sleep"
+
+Write a program that contains the following functions:
+1.	Calculates the length of the string 
+( int  stringLength( char string[ ]) 
+2.	Calculates the number of times 
+that a white space appears ( int numSpace( char string[])
+
+*/
+#include <stdio.h>
+
+//function prototypes
+int stringLength( char *);
+int numSpace(char *);
+
+int main(void)
+{
+	//character string from Shakespeare
+	char prose[] = {"To be or not to be - "\
+	"that is the question: Whether 'tis "\
+	"nobler in the mind to suffer the slings "\
+	"and arrows of outrageous fortune, "\
+	"or to take arms against a sea of "\
+	"troubles and, by opposing, end them. "\
+	"To die, to sleep"};
+	//define a pointer variable of type char to point to prose
+	char *prose_ptr = &prose[0];
+	
+	//create variables to store the results of the functions
+	int len = 0;
+	int spaces = 0;
+	
+	//print back the prose
+	printf("%s\n\n",prose);
+	
+	//calculate the length of the string
+	len = stringLength(prose_ptr);
+	
+	printf("string length is %i characters\n\n",len);
+	
+	//count the spaces in the string
+	spaces = numSpace(prose_ptr);
+	
+	printf(
+	"The number of spaces in the string is %i\n\n",spaces);
+		
+	return 0;
+}//end main
+
+int stringLength(char *string)
+{//function to calculate the chars in a string
+	int count = 0; 
+	
+	while((string[count])!='\0')	
+	{//start a while loop that counts elements
+		++count;
+	}//end while
+	
+	return count;
+}//end stringLength
+
+int numSpace(char *string)
+{
+	
+}//end numSpace
+	
+
+

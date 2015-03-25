f455a0b58c5ff54b8be0cb083647597d9bcdb1a0
Michael Lowry <mtl6@njit.edu>
2015-03-24 22:53:47 -0400

    Added to
    Added the secodn set of words so that one set of words was the same and
    another set of words was NOT the same

--- week6/cmpstr.cpp
+++ week6/cmpstr.cpp
@@ -5,30 +5,48 @@
 int compareStrings(char *string1,char *string2);
 
 int main(void)
-{
+{	
+	//define and interger to store the result of the comparison
+	int result;
 	//define two string literals
 	char word1[]="good";
-	char word2[]="goo";
+	char word2[]="good";
+	
+	//define a second pair of string literals
+	char word3[]="program";
+	char word4[]="programming";
 	
 	//define pointers of type char to each of the string literals
 	char *word1_ptr = &word1[0];
 	char *word2_ptr = &word2[0];
+	char *word3_ptr = &word3[0];
+	char *word4_ptr = &word4[0];
 	
+	////////FIRST SET OF WORDS
 	//print out the words
 	printf("The first word is %s. The second word is %s\n\n", word1, word2);
-	
-	//define and interger to store the result of the comparison
-	int result;
-	
 	//invoke the function to compare the strings
 	result = compareStrings(word1_ptr,word2_ptr);
-	
-	if(result){
+	if(result)
+	{
 		printf("The words %s and %s are the same\n\n",word1,word2);
 	}
 	else{
 		printf("The words %s and %s are NOT the same\n\n",word1,word2);
 	}
+	
+	////////SECOND SET OF WORDS
+	//print out the words
+	printf("The first word is %s. The second word is %s\n\n", word3, word4);
+	//invoke the function to compare the strings
+	result = compareStrings(word3_ptr,word4_ptr);
+	if(result)
+	{
+		printf("The words %s and %s are the same\n\n",word3,word4);
+	}
+	else{
+		printf("The words %s and %s are NOT the same\n\n",word3,word4);
+	}	
 	
 	return 0;
 }//end main
@@ -36,26 +54,28 @@ int main(void)
 int compareStrings (char *string1, char *string2)
 {
 	//declare a variable to hold the answer
-	int index,flag;
+	int /*index,*/flag;
 	printf("Entering the compareStrings Function...\n\n");
 	printf("The first word inside function: %s and %s\n\n",string1,string2);
 	
-	while(string1[index] == string2[index] && string1[index] != '\0' && string2[index] != '\0')
+	while(string1[0] == string2[0] && string1[0] != '\0' && string2[0] != '\0')
 	{
 		//print debug text
 		printf("Entering the loop...\n\n");
 		
-		++index;
+		//++index;
 		flag = 0;
-		
-		if(string1[index]==string2[index])
+		string1++;string2++;
+		if(string1[0]==string2[0])
 		{
+			printf("%c\t%c\t\n\n",string1[0],string2[0]);
 			flag = 1;//flag is 1 when words are the same
 		}//end if
 		else
 		{
+			printf("%c\t%c\t\n\n",string1[0],string2[0]);
 			flag = 0;//flag is 0 when the words are not the same
-		}//end else
+		}//end else 
 		
 	}//end while
 	printf("Exiting loop...\n\n");
@@ -65,8 +85,6 @@ int compareStrings (char *string1, char *string2)
 }//end compareStrings
 
 /*
-Output
-
 The first word is good. The second word is good
 
 Entering the compareStrings Function...
@@ -75,45 +93,65 @@ The first word inside function: good and good
 
 Entering the loop...
 
-index: 0        g       g
+o       o
 
 Entering the loop...
 
-index: 1        o       o
+o       o
 
 Entering the loop...
 
-index: 2        o       o
+d       d
 
 Entering the loop...
 
-index: 3        d       d
+
 
 Exiting loop...
 
 The words good and good are the same
 
-
---------------------------------
-Process exited after 0.04325 seconds with return value 0
-Press any key to continue . . .
-
-
-The first word is good. The second word is bad
+The first word is program. The second word is programming
 
 Entering the compareStrings Function...
 
-The first word inside function: good and bad
+The first word inside function: program and programming
 
 Entering the loop...
 
-index: 0        g       b
+r       r
 
-The words good and bad are NOT the same
+Entering the loop...
+
+o       o
+
+Entering the loop...
+
+g       g
+
+Entering the loop...
+
+r       r
+
+Entering the loop...
+
+a       a
+
+Entering the loop...
+
+m       m
+
+Entering the loop...
+
+        m
+
+Exiting loop...
+
+The words program and programming are NOT the same
 
 
 --------------------------------
-Process exited after 0.05382 seconds with return value 0
+Process exited after 0.04249 seconds with return value 0
 Press any key to continue . . .
 */
 
